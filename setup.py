bee2e71b1503532f83ab56ee2805461315fda021
geoyee <geoyee@yeah.net>
2022-08-28 23:30:46 +0800

    feat(all): init

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+with open("requirements.txt") as fin:
+    REQUIRED_PACKAGES = fin.read()
+
+setuptools.setup(
+    name="sdrl",
+    version="0.0.1",
+    author="geoyee",
+    author_email="geoyee@yeah.net",
+    description="Satellite Data Read Library",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geoyee/sdrl",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=REQUIRED_PACKAGES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
