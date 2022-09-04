34e35c2321cf40355f93e53246060a8bd3b584ed
geoyee <geoyee@yeah.net>
2022-09-04 18:14:51 +0800

    fix(config): path bugfix

--- sdrl/config/base.py
+++ sdrl/config/base.py
@@ -2,12 +2,15 @@ import os.path as osp
 from glob import glob
 
 
+PATH = osp.dirname(osp.abspath(__file__))
+
+
 def get_file_name(filr_path: str) -> str:
     return filr_path.replace("\\", "/").split("/")[-1].split(".")[0]
 
 
 SATELLITES = {}
-satellite_dir = "sdrl/config/satellite"
+satellite_dir = osp.join(PATH, "satellite")
 yamls = glob(osp.join(satellite_dir, "*/*.yaml"))
 for yaml_file in yamls:
     name = get_file_name(yaml_file)
