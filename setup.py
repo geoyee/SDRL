39d0afe6ae017db040ceb9a94504598362b48a5a
geoyee <geoyee@yeah.net>
2022-09-04 14:40:56 +0800

    fix(setup): bugfix

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
+import os.path as osp
+import pathlib
 import setuptools
 
 
-with open("README.md", "r") as fh:
+PATH = pathlib.Path(__file__).parent
+
+with open(osp.join(PATH, "README.md"), "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
-with open("requirements.txt", "r") as fin:
+with open(osp.join(PATH, "requirements.txt"), "r") as fin:
     REQUIRED_PACKAGES = fin.read()
 
 setuptools.setup(
     name="sdrl",
-    version="0.0.2",
+    version="0.0.1",
     author="geoyee",
     author_email="geoyee@yeah.net",
     description="Satellite Data Read Library",
